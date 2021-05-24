b60a46c556d765e4585f91ac20d91fe7acd24826
Ashanti Long <ashantilong15@gmail.com>
2021-05-24 09:41:30 -0400

    New updates

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-var myVar = '' ;
+var myVar = '';
 
 function startTimer(duration, display) {
-    var timer = duration, minutes, seconds;
+    var timer = duration,
+        minutes,
+        seconds;
     myVar = setInterval(function () {
         minutes = parseInt(timer / 60, 10)
         seconds = parseInt(timer % 60, 10);
@@ -11,56 +13,104 @@ function startTimer(duration, display) {
 
         display.textContent = minutes + ":" + seconds;
 
-        if (--timer < 0) {
+        if (-- timer < 0) {
             timer = duration;
         }
     }, 1000);
 }
 
 function Start() {
-	
-	var display = document.querySelector('#timer');
+
+    var display = document.querySelector('#timer');
     var currentButton = document.getElementById('start').innerHTML;
     var currentTime = display.textContent;
     console.log(currentTime);
-    if (currentButton === "Start"){
+    if (currentButton === "Start") {
         var minutes = 60 * currentTime.replace(':', '.');
         document.querySelector('#start').innerHTML = 'Stop';
 
         startTimer(minutes, display);
-        
-    }
-    else {
+
+    } else {
         document.querySelector('#start').innerHTML = 'Start';
         clearInterval(myVar);
 
-        //currrentTime = display;
-        
-    }   
+        // currrentTime = display;
+
+    }
 
 
 }
 
 
-function Pomodoro(data){
+function Pomodoro(data) {
 
     document.getElementById('timer').innerHTML = data + ':00';
-    if (data == 25){
-        document.body.style.backgroundColor = 'rgb(219, 82, 77)' ;
-    }
-    else if (data == 5){
-        document.body.style.backgroundColor = 'rgb(70, 142, 145)' ;
-    }
-    else {
+    if (data == 25) {
+        document.body.style.backgroundColor = 'rgb(219, 82, 77)';
+    } else if (data == 5) {
+        document.body.style.backgroundColor = 'rgb(70, 142, 145)';
+    } else {
 
-        document.body.style.backgroundColor = 'rgb(67, 126, 168)' ;
+        document.body.style.backgroundColor = 'rgb(67, 126, 168)';
     }
-    
+
 
 }
 
-/*
-1 - Find out why it's lagging (Hint - Display)
-2 - Get Stop to work (Hint - Value of button changes but not the ID)
-3 - Make button throw a parameter depending on current time (EX: if current time is 25:00 then throw 25:00 into Start function 
-*/
+function addTask() {
+    document.getElementById("addTask").style.display = "none"
+    document.getElementById("card").style.display = "block";
+
+
+}
+
+function cancelTask() {
+    document.getElementById("addTask").style.display = "block"
+    document.getElementById("card").style.display = "none";
+    document.getElementById("inputEst").value = 0;
+
+
+}
+
+function inc() {
+    var est = parseInt(document.getElementById("inputEst").value);
+    if (est < 25) {
+        document.getElementById("inputEst").value = est + 1;
+    }
+
+
+}
+
+function dec() {
+    var est = parseInt(document.getElementById("inputEst").value);
+    if (est > 0) {
+        document.getElementById("inputEst").value = est - 1;
+    }
+
+}
+
+var task = [];
+function save(){
+var item = document.getElementById("placeholder").value;
+task.push(item);
+
+if (task.length > 0){
+    document.getElementById('currentTask').innerHTML = "";
+
+    for (var i =0 ; i < task.length; i++){
+        var node = document.createElement("div");
+        node.className = "taskStyle";
+        node.innerHTML = "<i class='fa fa-check iconStyle' aria-hidden='true'></i><span id = "+i+">"+task[i]+"</span>";
+        //document.getElementById('currentTask').innerHTML = ""
+        document.getElementById('currentTask').appendChild(node);
+      
+
+      
+    }
+    document.getElementById("addTask").style.display = "block";
+    document.getElementById("card").style.display = "none";
+    document.getElementById("inputEst").value = 0;
+    document.getElementById("placeholder").value = "";
+}
+}
